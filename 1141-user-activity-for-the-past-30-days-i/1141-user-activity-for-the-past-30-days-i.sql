7beda87d725e32a7c66985120036e9df1e0ba528
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2024-05-12 16:56:39 +0200

    Time: 981 ms (81.63%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
 SELECT TO_CHAR(activity_date, 'YYYY-MM-DD') AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date BETWEEN ADD_MONTHS('2019-07-28', -1) AND '2019-07-27'
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date
 ORDER BY day;
