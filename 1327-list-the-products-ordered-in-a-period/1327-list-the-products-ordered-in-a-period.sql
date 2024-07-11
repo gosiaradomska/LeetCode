c687d6c0105d66dff153377e760fffb250120c47
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2024-07-11 23:18:57 +0200

    Time: 855 ms (62.53%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,7 @@
 SELECT product_name, SUM(unit) AS unit FROM Products
 INNER JOIN Orders
 ON Products.product_id = Orders.product_id
-WHERE  EXTRACT(month FROM order_date) = 2 AND  EXTRACT(year FROM order_date) = 2020
+WHERE EXTRACT(month FROM order_date) = 2 AND  EXTRACT(year FROM order_date) = 2020
 GROUP BY product_name, EXTRACT(month FROM order_date)
 HAVING SUM(unit) >= 100;
 
