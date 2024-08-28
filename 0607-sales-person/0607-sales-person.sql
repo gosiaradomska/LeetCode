ce91ac8da220ffca861e26a2927533ca8d7008f0
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2024-08-28 17:09:10 +0200

    Time: 1216 ms (85.48%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,10 @@
+SELECT name
+FROM SalesPerson
+WHERE name NOT IN(
+    SELECT SalesPerson.name
+    FROM SalesPerson
+    LEFT JOIN Orders
+    ON SalesPerson.sales_id = Orders.sales_id
+    LEFT JOIN Company
+    ON Orders.com_id = Company.com_id
+    WHERE Company.name = 'RED');
