6c57bbb43ccc89d65127f2cabe5e914b84846f4d
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2024-08-28 18:00:43 +0200

    Time: 1494 ms (13.53%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,10 +1,8 @@
-SELECT DISTINCT p.product_id, product_name
+SELECT p.product_id, p.product_name
 FROM Product p
-WHERE p.product_id NOT IN (
-    SELECT p.product_id
-    FROM Product p
-    LEFT JOIN Sales s
-    ON p.product_id = s.product_id
-    WHERE (sale_date < '2019-01-01' OR sale_date > '2019-03-31')
-    OR sale_date IS NULL);
+LEFT JOIN Sales s
+ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01'
+AND MAX(s.sale_date) <= '2019-03-31';
 
