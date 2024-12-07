82548978734201b2d8c176d77f0d86b9a7e9dea7
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2024-12-07 17:43:00 +0100

    Time: 394 ms (98.68%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,9 +1,9 @@
 SELECT 
-    TO_CHAR(TO_DATE(sell_date, 'YYYY-MM-DD'), 'YYYY-MM-DD') AS sell_date, 
+    sell_date, 
     COUNT(product) AS num_sold, 
-    LISTAGG(product, ',') WITHIN GROUP (ORDER BY product) AS products 
+    GROUP_CONCAT(product ORDER BY product SEPARATOR ',') AS products 
 FROM (
     SELECT DISTINCT sell_date, product
     FROM Activities
-) 
+) sub
 GROUP BY sell_date;
