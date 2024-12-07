b5dda821a54afc383e84435cf95392a686ff10d9
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2024-12-07 16:43:49 +0100

    Time: 1991 ms (6.77%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
-/* Write your PL/SQL query statement below */
-SELECT unique_id, name FROM EmployeeUNI
-RIGHT JOIN Employees
-ON EmployeeUNI.id = Employees.id;
+SELECT unique_id, name 
+FROM Employees
+LEFT JOIN EmployeeUNI
+ON Employees.id = EmployeeUNI.id;
