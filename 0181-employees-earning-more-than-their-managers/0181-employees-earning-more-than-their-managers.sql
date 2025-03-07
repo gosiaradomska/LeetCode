59de5d68858cad6beeb46f1628cf19f076bb95e4
Małgorzata Radomska <163201690+gosiaradomska@users.noreply.github.com>
2025-03-08 00:19:20 +0100

    Time: 769 ms (42.57%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,5 @@
-SELECT e1.name AS Employee FROM employee e1
-INNER JOIN employee e2
+SELECT e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2
 ON e1.managerId = e2.id
-WHERE e1.salary > e2.salary;
+WHERE e2.salary < e1.salary;
